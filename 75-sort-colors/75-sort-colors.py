9b63e9d203b3f92275fa05815b7cc7e88cf88d1c
GelilaT <101528976+GelilaT@users.noreply.github.com>
2022-09-29 13:15:45 +0300

    Time: 44 ms (17.74%), Space: 13.4 MB (64.55%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def sortColors(self, nums):
+        for i in range(len(nums)-1):
+     
+          minIndex = i  
+          
+          for j in range(i+1,len(nums)):  
+            if nums[j]<nums[minIndex]:  
+                minIndex = j  
+                  
+          nums[i], nums[minIndex] = nums[minIndex], nums[i]  
+        return nums
