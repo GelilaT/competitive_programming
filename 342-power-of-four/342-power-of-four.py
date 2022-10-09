19c9d3b48a2567bfb6ad76eedc7abe94111884ec
GelilaT <101528976+GelilaT@users.noreply.github.com>
2022-10-09 23:41:00 +0300

    Time: 67 ms (12.87%), Space: 13.8 MB (94.91%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n<=0:
+            return False
+        elif n==1:
+            return True
+        else:
+          while n%4==0:
+            n=n/4
+            if n==1:
+                return True
+          return False
+          
+        
