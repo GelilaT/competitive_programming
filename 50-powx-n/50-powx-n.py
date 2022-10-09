a9fd8c761ce809e6d928c31cf8f6ed84de9bc79a
GelilaT <101528976+GelilaT@users.noreply.github.com>
2022-10-09 23:21:51 +0300

    Time: 31 ms (94.59%), Space: 13.9 MB (20.05%) - LeetHub

---
+++ 50-powx-n/50-powx-n.py
@@ -0,0 +1,18 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        new=abs(n)
+        def changer(x,new):
+            if x==0:
+                return 0   
+            elif new==0:
+                return 1
+            result=changer(x,new//2)
+            result=result*result
+            if new%2:
+                return result*x
+            return result
+        result=changer(x,new)
+        if new!=n:
+            return 1/result
+        return result
+        
