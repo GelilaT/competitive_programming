6adecd4af5ae0719c6956e1fabb3529f523a8029
GelilaT <101528976+GelilaT@users.noreply.github.com>
2022-11-11 17:19:35 +0300

    Time: 157 ms (55.49%), Space: 14.4 MB (88.13%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,11 +1,11 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-          road=[0]*1001
-          for passenger,start,end in trips:
+        road=[0]*1001
+        for passenger,start,end in trips:
             road[start]+=passenger
             road[end]-=passenger
-          for i in range(0,len(road)):
+        for i in range(0,len(road)):
             road[i]=road[i]+road[i-1]
             if road[i]>capacity:
                 return False
-          return True
+        return True
