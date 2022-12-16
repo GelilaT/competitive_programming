d1b8e1b5bbb60f8206f5f0a230175318aa8042a7
GelilaT <101528976+GelilaT@users.noreply.github.com>
2022-12-16 19:34:52 +0300

    Update 0125-valid-palindrome.py

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-         newstr=""
-         for c in s:
-            if c.isalnum():
-                newstr+=c.lower()
-         return newstr==newstr[::-1]
+        left=0
+        right=len(s)-1
+        s=s.lower()
+        while left<=right:
+            if s[left].isalnum():
+                if s[right].isalnum():
+                    if s[left]!=s[right]:
+                        return False
+                    left+=1
+                    right-=1
+                else:
+                   right-=1
+            else:
+                left+=1
+        return True
