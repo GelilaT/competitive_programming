9988a01fc5ba95cf8585b1d4ef03698966bfc158
GelilaT <101528976+GelilaT@users.noreply.github.com>
2022-12-23 13:54:18 +0300

    Create missing_number.py

---
+++ missing_number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        length=len(nums)
+        for num in range(length+1):
+            if num not in nums:
+                return num
+                
