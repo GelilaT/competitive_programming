c6e802007f6569661bfa8c5b2f5ac49db520c4ef
GelilaT <101528976+GelilaT@users.noreply.github.com>
2023-01-02 15:35:23 +0300

    Create simple bank system.py

---
+++ simple bank system.py
@@ -0,0 +1,25 @@
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance=balance
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if self.withdraw(account1,money):
+            if self.deposit(account2,money):
+                return True
+            self.deposit(account1,money)
+        else:
+            return False
+    def deposit(self, account: int, money: int) -> bool:
+        if 1<=account<=len(self.balance):
+            self.balance[account-1]+=money
+            return True
+        else:
+            return False
+    def withdraw(self, account: int, money: int) -> bool:
+        if 1<=account<=len(self.balance):
+            if self.balance[account-1]>=money:
+               self.balance[account-1]-=money
+               return True
+            else:
+                return False 
