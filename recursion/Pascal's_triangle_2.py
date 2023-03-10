6357c00e9cbea69e0d261e7ff62eb388da18cd11
GelilaT <101528976+GelilaT@users.noreply.github.com>
2023-03-10 15:17:43 +0300

    Create Pascal's_triangle_2.py

---
+++ recursion/Pascal's_triangle_2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        else:
+            prev = self.getRow( rowIndex - 1 )
+            current = [1]
+
+            for i in range(1 , rowIndex):
+                current.append(prev[i - 1] + prev[i])
+            current.append(1)
+
+            return current 
+            
