f698505ded942ebd0d4689d6f9921d4f0d0f15fc
GelilaT <101528976+GelilaT@users.noreply.github.com>
2023-05-07 14:09:11 +0300

    added top-k-frequent-words.python

--- top-k-frequent-words.py
+++ top-k-frequent-words.py
@@ -7,10 +7,5 @@ class Solution:
         ans = []
         print(heap)
         for i in range(k):
-            if ans and count[ans[-1]] == count[heap[0][1]] and ans[-1] > heap[0][1]:
                 ans.append(heappop(heap)[1])
-                ans[-2], ans[-1] = ans[-1], ans[-2]
-            else:
-                ans.append(heappop(heap)[1])
-
         return ans
