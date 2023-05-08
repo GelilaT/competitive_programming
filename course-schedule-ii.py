2a07ec0f27fd63df7470ba5b0b73894e4ba74367
GelilaT <101528976+GelilaT@users.noreply.github.com>
2023-05-08 19:56:00 +0300

    added course-schedule-ii.python

--- course-schedule-ii.py
+++ course-schedule-ii.py
@@ -1,26 +1,30 @@
 class Solution:
     def findOrder(self, n: int, pre: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
-        in_degree = [0] * n
+        color = [0] * n
         for i in range(len(pre)):
             graph[pre[i][1]].append(pre[i][0])
-            in_degree[pre[i][0]] += 1
-        
-        queue = deque()
-        for i in range(n):
-            if in_degree[i] == 0:
-                queue.append(i)
-        
-        courses = []
-        while queue:
-            node = queue.popleft()
-            courses.append(node)
+
+        ans = []
+        cyclic = False
+        def dfs(node):
+            nonlocal color
+            nonlocal ans
+            nonlocal cyclic
             for adj in graph[node]:
-                in_degree[adj] -= 1
-                if in_degree[adj] == 0:
-                    queue.append(adj)
-                
-        if len(courses) == n:
-            return courses
-        else:
-            return []
+                if color[adj] == 0:
+                    color[adj] = 1
+                    dfs(adj)
+                elif color[adj] == 1:
+                    cyclic = True
+                    return 
+             
+            color[node] = 2
+            ans.append(node)
+        
+        for i in range(n):
+            if cyclic:
+                return []
+            if color[i] == 0:
+                dfs(i)
+        return ans[::-1]
