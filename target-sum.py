44765dc882eea65d9b9829a4aa47ef185087f9d7
GelilaT <101528976+GelilaT@users.noreply.github.com>
2023-06-25 20:18:18 +0300

    added target-sum.python

---
+++ target-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+        result = 0
+        def findTarget(index, n):
+
+            nonlocal result
+            
+            if index == len(nums):
+                if n == target: 
+                    return 1
+                else:
+                    return 0
+
+            if (n, index) in memo:
+                return memo[(n, index)]
+            
+            result = findTarget(index + 1, n - nums[index]) + findTarget(index + 1, n + nums[index])
+                
+            memo[(n, index)] = result
+            return result
+
+        findTarget(0, 0)
+        return result
