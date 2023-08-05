6e8cf1b862084e3903451094bb9d7a01b0177cb7
GelilaT <101528976+GelilaT@users.noreply.github.com>
2023-08-05 17:53:18 +0300

    added reducing-dishes.python

---
+++ reducing-dishes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        satisfaction.sort()
+        n = len(satisfaction)
+        ans, sum, coef = 0, 0, 0
+
+        for i in range(n - 1, -1, -1):
+            if sum + satisfaction[i] <= 0:
+                break
+            
+            sum += satisfaction[i]
+            coef += sum
+            ans = max(ans, coef)
+
+        return ans
