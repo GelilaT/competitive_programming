e152f20af2dce5c679d64c6612482496f4457198
GelilaT <101528976+GelilaT@users.noreply.github.com>
2023-09-19 12:38:11 +0300

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,46 +1,25 @@
 class Solution:
     def minSteps(self, x: int) -> int:
-        ops = 0
-        i = 2
-        while i * i <= x:
-            while x % i == 0:
-                ops += i
-                x //= i
-            i += 1
+
+        memo = {}
+        if x == 1:
+            return 0
+
+        def minOp(op, i, total):
+
+            if total == x:
+                return 0
+
+            if total > x:
+                return float('inf')
+
+            if (op, i, total) not in memo:
+
+                if op == 'P':
+                    memo[(op, i, total)] = min(minOp('P', i, total + i), minOp('C', total, total)) + 1
+                else:
+                    memo[(op, i, total)] = minOp('P', i, total + i) + 1
+            
+            return memo[(op, i, total)]
         
-        if x != 1:
-            ops += x
-        
-        return ops
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # else:
-        #     count = 0
-        #     while x > 0:
-        #         n = 2
-        #         while n * 2 <= x:
-        #             n *= 2
-        #         count += 2
-        #         x -= n
-        #     return count
+        return minOp('C', 1, 1) + 1
