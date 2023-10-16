5ed748b20abfe4d54e88c8c59fc90a7173aaf223
GelilaT <101528976+GelilaT@users.noreply.github.com>
2023-10-16 14:35:35 +0300

    added uncrossed-lines.python

---
+++ uncrossed-lines.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        
+        @cache
+        def dp(i, j):
+
+            if i == len(nums1) or j == len(nums2):
+                return 0
+
+            if nums1[i] == nums2[j]:
+                return 1 + dp(i + 1, j + 1)
+
+            return max(dp(i, j + 1), dp(i + 1, j))
+
+        return dp(0, 0)
