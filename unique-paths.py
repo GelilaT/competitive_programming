0cb644ea4dd82b44bdd2d750616bc66f400cbf42
GelilaT <101528976+GelilaT@users.noreply.github.com>
2023-10-16 14:06:10 +0300

    added unique-paths.python

--- unique-paths.py
+++ unique-paths.py
@@ -4,12 +4,16 @@ class Solution:
         def inbound(row, col):
             return 0 <= row < m and 0 <= col < n
 
-        arr = [[0 for _ in range(n)]for i in range(m)]
-        arr[m - 1][n - 1] = 1
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                if inbound(i, j + 1):
-                    arr[i][j] += arr[i][j + 1]
-                if inbound(i + 1, j):
-                    arr[i][j] += arr[i + 1][j]
-        return arr[0][0]
+        arr = [[0 for _ in range(n)] for i in range(m)]
+        arr[0][0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+
+                if inbound(i, j - 1):
+                    arr[i][j] += arr[i][j - 1]
+
+                if inbound(i - 1, j):
+                    arr[i][j] += arr[i - 1][j]
+
+        return arr[-1][-1]
