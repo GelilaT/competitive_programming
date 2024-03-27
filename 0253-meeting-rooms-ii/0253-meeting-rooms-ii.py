08216dd49400e2fd4e44531beb5818169f17f062
GelilaT <101528976+GelilaT@users.noreply.github.com>
2024-03-27 09:58:15 +0300

    Time: 66 ms (92.01%), Space: 19.8 MB (97.60%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,16 +1,15 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        maxi = 0
+        heap = []
+        intervals.sort()
+        count = 0
+        
         for start, end in intervals:
-            maxi = max(maxi, end)
+            while heap and start >= heap[0]:
+                heappop(heap)
+                
+            heappush(heap, end)
+            count = max(count, len(heap))
             
-        prefix = [0] * (maxi + 1)
-        for start, end in intervals:
-            prefix[start] += 1
-            prefix[end] -= 1
-            
-        for i in range(1, maxi):
-            prefix[i] = prefix[i] + prefix[i - 1]
-            
-        return max(prefix)
+        return count
