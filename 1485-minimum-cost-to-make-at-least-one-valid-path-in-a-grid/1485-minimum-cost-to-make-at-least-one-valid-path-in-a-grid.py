e5712162369a656e3305afb41d3b5574bf120731
Gelila Tefera <101528976+GelilaT@users.noreply.github.com>
2025-01-18 20:02:37 +0300

    Time: 93 ms (89.61%), Space: 19 MB (72.08%) - LeetHub

---
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def minCost(self, grid):
+        
+        m, n = len(grid), len(grid[0])
+        minCost = [[float('inf')] * n for _ in range(m)]
+        minCost[0][0] = 0
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        queue = deque([(0, 0)])
+        
+        
+        while queue:
+            r, c = queue.popleft()
+            
+            for i, (dr, dc) in enumerate(directions):
+                nr, nc = r + dr, c + dc
+                cost = 1 if grid[r][c] != i + 1 else 0
+                
+                if 0 <= nr < m and 0 <= nc < n and minCost[r][c] + cost < minCost[nr][nc]:
+                    minCost[nr][nc] = minCost[r][c] + cost
+                    
+                    if cost == 1:
+                        queue.append((nr, nc))
+                    else:
+                        queue.appendleft((nr, nc))
+        
+        return minCost[m - 1][n - 1]
+        
