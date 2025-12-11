cefdb22669999cd23a8b7c9dc01a4e987b3bb5c7
Gelila Tefera <101528976+GelilaT@users.noreply.github.com>
2025-12-11 16:50:01 +0300

    Time: 8 ms (79.17%), Space: 18 MB (31.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,5 @@
 class Solution:
-    def isAnagram(self, s1: str, s2: str) -> bool:
-         from collections import Counter
-         ci=Counter(s1)
-         if len(s1)!=len(s2):
-             return False
-         return Counter(s1)==Counter(s2)
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        return Counter(s) == Counter(t)
+        
